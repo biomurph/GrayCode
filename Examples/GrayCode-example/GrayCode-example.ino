504961be851d88cb64a4d7ec50e5e7e4553c9068
Joel Murphy <biomurph@gmail.com>
2024-09-18 16:24:09 -0400

    added example sketch

---
+++ Examples/GrayCode-example/GrayCode-example.ino
@@ -0,0 +1,35 @@
+
+
+#include "GrayCode.h"
+
+#define NUM_ENCODERS 2
+#define SHIFT_IN 7
+#define SHIFT_LOAD 3
+#define SHIFT_CLK 2
+
+GrayCode gray(NUM_ENCODERS, SHIFT_LOAD, SHIFT_CLK, SHIFT_IN);
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial){}
+  delay(500);
+  Serial.println("Gray Code Encoder Example");
+  
+  if(!gray.begin()){
+    Serial.println("Memory allocation failed");
+  }
+  
+}
+
+void loop() {
+
+  if(gray.checkPositions()){
+    for(int i=0; i<NUM_ENCODERS; i++){
+      Serial.print(gray.pos[i],HEX);
+      Serial.print("\t");
+    }
+  }
+  delay(10);
+
+}
+
